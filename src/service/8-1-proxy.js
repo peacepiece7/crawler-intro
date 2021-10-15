62f025386886cc37fb851591bbe34aa2d9ebcd79
taetae <peacepiece7@naver.com>
2021-10-15 16:26:13 +0900

    proxy setting

---
+++ src/service/8-1-proxy.js
@@ -0,0 +1,66 @@
+const puppeteer = require("puppeteer");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const cralwer = async () => {
+  try {
+    let browser = await puppeteer.launch({ headless: false, args: ["--window-size=1280,1280"] });
+    await browser.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36");
+    let page = await browser.newPage();
+    page.setViewport({
+      width: 1080,
+      height: 1080,
+    });
+    await page.goto("https://spys.one/en/free-proxy-list/");
+
+    const proxies = await page.evaluate(() => {
+      const ips = [...document.querySelectorAll(".spy14:first-child .spy2")].map((element) => {
+        return element.parentNode.textContent.replace(/document\.write\(.+\)/, "");
+      });
+      const types = [...document.querySelectorAll("tr td:nth-of-type(2)")]
+        .filter((v, i) => {
+          if (i > 2) {
+            return v;
+          }
+        })
+        .map((v) => {
+          return v.textContent;
+        });
+      const latencies = Array.from(document.querySelectorAll("tr td:nth-of-type(6) .spy1")).map((element) => {
+        return element.textContent;
+      });
+
+      return ips.map((value, idx) => {
+        return {
+          ip: value,
+          type: types[idx],
+          latency: latencies[idx],
+        };
+      });
+    });
+    console.log(proxies.filter((val) => console.log(val.type, val.type)));
+    const filtered = proxies.filter((value) => value.type.split(" ")[0] === "HTTP").sort((pre, cur) => pre.latency - cur.latency);
+    console.log(`--proxy-server=http://${filtered[0].ip}`);
+
+    await page.setJavaScriptEnabled(false);
+    page.setDefaultNavigationTimeout(20000);
+
+    await page.close();
+    await browser.close();
+    console.log(filtered[0].ip);
+    browser = await puppeteer.launch({ headless: false, args: ["--window-size:1280,1280", `--proxy-server=http://${filtered[0].ip}`] });
+    page = await browser.newPage();
+    await page.goto("http://www.whatismyproxy.com/");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+cralwer();
+
+// SOCKS5 = Deep web browser header
+// HIA = High Annonimity
+// NOA = Not of Annonimity
+
+// https://spys.one/en/free-proxy-list/
+// postman으로 보냈을 떄, 페이지 정보가 잘 나오면 크롤링하기 쉬운 사이트
