8df113a17d9b752271848fdb60f6460bc8a0ad8e
taetae <peacepiece7@naver.com>
2021-10-18 16:57:36 +0900

    재업로드

--- src/service/almacielo-crawler.js
+++ src/service/almacielo-crawler.js
@@ -27,9 +27,7 @@ const crawler = async (siteName) => {
       args: ["--window-size:1280,1280"],
       defaultViewport: { width: 900, height: 1080 },
     });
-    await browser.userAgent(
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
-    );
+    await browser.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36");
     const newUrlList = [];
     let page = await browser.newPage();
     for (const [i, r] of Object.entries(records)) {
