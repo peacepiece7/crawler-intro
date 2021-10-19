c05cead9bcf21564f5298cb3f8801a89619fa860
taetae <peacepiece7@naver.com>
2021-10-19 13:32:07 +0900

    tar uploader

--- src/service/tar-upload.js
+++ src/service/tar-upload.js
@@ -14,25 +14,45 @@ const crawler = async (provider) => {
     // * provider 검색
     await page.goto("http://115.22.68.60/master/uphtml_sub_all.jsp");
     await page.waitForSelector("select");
-    await page.type("select", provider);
-    await page.waitForTimeout(2000);
-    await page.click("input[name=cc]");
+    await page.waitForTimeout(888);
 
-    await page.waitForSelector("select");
-    await page.waitForTimeout(2000);
+    let bool = true;
+    while (bool) {
+      await page.type("select", provider);
+      await page.waitForTimeout(2000);
+      await page.click("input[name=cc]");
 
-    // * Detail Icon Click
-    await page.evaluate(() => {
-      const icons = document.querySelectorAll("tr td:nth-child(5) a");
-      icons &&
-        Array.from(icons).forEach((icon, idx) => {
-          if (idx >= 2) {
-            setTimeout(() => {
+      await page.waitForSelector("select");
+      await page.waitForTimeout(1244);
+
+      // * Detail Icon Click
+      await page.evaluate(() => {
+        const icons = document.querySelectorAll("tr td:nth-child(5) a");
+        icons &&
+          Array.from(icons).forEach((icon, idx) => {
+            if (idx >= 2) {
               icon.click();
-            }, 1500 * idx);
-          }
-        });
-    });
+            }
+          });
+      });
+
+      await page.waitForTimeout(2000);
+
+      const partNumber = await page.evaluate(() => {
+        return document.querySelector("tr td:nth-child(1) > a").textContent;
+      });
+
+      if (partNumber) {
+        const text = `C:\\Users\\INTERBIRD\\Desktop\\tar\\${partNumber}.tgz`;
+        const input = await page.$("input[type=file]");
+        await input.uploadFile(text);
+        await page.keyboard.down("Control");
+        await page.click("input[name=B1]");
+        await page.keyboard.up("Control");
+      } else {
+        bool = false;
+      }
+    }
   } catch (err) {
     console.log(err);
   }
